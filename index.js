33374b4b7ac21d66d2cfdad66254a7be5cd1be63
Tej Narayan Singh <tejnarayan950@gmail.com>
2021-11-27 13:24:33 +0530

    New slots has been added

--- index.js
+++ index.js
@@ -1,11 +1,109 @@
 const exp = require('express');
 const app = exp();
 const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
 
 app.use(bodyParser.urlencoded({extended:true}));
 app.set("view engine","ejs");
 app.use(exp.static('public'));
 
+// ================================================================================================
+
+var department = ['Otolaryngologist','Immunologists','Dermatologist','Hepatologist','Allergist','Rheumatologist','Pulmonologist',
+'Orthopaedists','Infectious disease specialist','Hepatologist','General physician','General physician','Endocrinologist',
+'Gastroenterologist','Allergist','Infectious disease specialist','Otolaryngologist','Gastroenterologist','Cardiologist','Hepatologist',
+'Hepatologist','Hepatologist','Hepatologist','General practitioner','Endocrinologist','Endocrinologist',
+'Endocrinologist','General practitioner','Gastroenterologist','Infectious disease specialist','General practitioner',
+'Orthopaedician','Neurologist','Gastroenterologist','Pulmonologist','Dermatologist','Infectious disease specialist',
+'General practitioner','Urologist','Cardiologist','Hepatologist','General Physician','Physical Trainer'
+];
+
+// ================================================================================================
+
+mongoose.connect('mongodb+srv://tej:tej@cluster0.uxnrl.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',{
+// mongoose.connect('mongodb://localhost:27017/user',{
+    useNewUrlParser:true,
+    useUnifiedTopology:true
+})
+.then(()=>{console.log("Database Added");})
+.catch(err=>{console.log(err,'this is error');})
+
+////    User Database
+var UserSchema = new mongoose.Schema({
+    name:String,
+    age:Number,
+    email:String,
+    sex:String,
+    height:Number,
+    weight:Number,
+    temp:Number,
+    dob:String,
+    sm:Number,
+    al:Number,
+    ph:Number,
+    bmi:Number,
+    ubp:Number,
+    lbp:Number,
+    chol:String,
+    gulc:String,
+    gul:Number,
+    ch:Number,
+    gd:Number,
+    ag:Number,
+    book1:String,
+    book2:String,
+    bookphy:String,
+    booktrain:String
+});
+var User = mongoose.model("User",UserSchema);
+
+// ////    Slot Database
+// var slotSchema = new mongoose.Schema({
+//     '10:00 AM' : {type:Boolean, default:false},
+//     '10:30 AM' : {type:Boolean, default:false},
+//     '11:00 AM' : {type:Boolean, default:false},
+//     '11:30 AM' : {type:Boolean, default:false},
+//     '12:00 NOON' : {type:Boolean, default:false},
+//     '12:30 PM' : {type:Boolean, default:false},
+//     '01:00 PM' : {type:Boolean, default:false},
+//     '01:30 PM' : {type:Boolean, default:false},
+//     '03:00 PM' : {type:Boolean, default:false},
+//     '03:30 PM' : {type:Boolean, default:false},
+// });
+// var Slot = mongoose.model('slot',slotSchema);
+
+////    Department Database
+var AppointmentSchema = new mongoose.Schema({
+    Deprt: String,
+    slot: [{type:Number}]
+});
+var Appoint = mongoose.model('appoint',AppointmentSchema);
+
+p = [];
+department.forEach(element => {
+    y = p.find((x)=>x===element);
+    if(y){}
+    else{
+        p.push(element);
+        Appoint.find({Deprt:""+ element},(err,f)=>{
+            if(f.length!==0){}
+            else{
+                Appoint.create({Deprt:element},()=>{});
+            }
+        })
+    }
+});
+// Appoint.create({Deprt:"Radio"},()=>{});
+// Appoint.findOne({Deprt:"Radio"},function(err,found){
+//     found.slot.push(2);
+//     found.save(()=>{});
+// });
+
+////    New Symptoms Database
+var sympSchema = new mongoose.Schema({symptom:String});
+var Symp = mongoose.model('symps',sympSchema);
+
+// ================================================================================================
 function getAge(dateString) {
     var today = new Date();
     var birthDate = new Date(dateString);
@@ -17,90 +115,128 @@ function getAge(dateString) {
     return age;
 }
 
+function CTime(x){
+    sol = '';
+    if(x>10)sol='03:/0 PM';
+    else if(x>8)sol='02:/0 PM';
+    else if(x>6)sol='01:/0 PM';
+    else if(x>4)sol='12:/0 PM';
+    else if(x>2)sol='11:/0 AM';
+    else sol='10:00 AM';
+    if(x==2||x==4||x==6||x==8||x==10||x==12){sol = sol.replace('/','3');}
+    else {sol = sol.replace('/','0');}
+    return sol;
+}
+
 var name="",age=0,email="",sex="",hgt=0,wgt=0,temp=0,dob="",smoke="",alcohol="",physc="",bmi=0;
 var ag=50*365,hg=170,wi=70,gd=2,sb=120,db=80,ch=1,gl=1,sm=1,al=1,ph=1;
 var ubp=0,lbp=0,gul=0,chol=0;
 var data2=0;
-// age in days
-// height in cm
-// weight in float kg
-// gender 1-female 2-male
-// systolic,dias int
-// chol, gluse 1-normal 2-abv 3-welanv
-// smoke, alcol, activ 0-no 1-yes
+
 var data=[];var ab=[];
 var perc=[];
+
+// ================================================================================================
 
 app.get('/',(req,res)=>{res.render("home.ejs");})
 
 app.get('/detail',(req,res)=>{res.render("detail.ejs");})
-
-app.post('/gentest',(req,res)=>{
-    var gender = req.body;
-    if(gender.male=='male')sex='male';
-    else sex='female';
-    console.log(sex);
-    res.redirect("/gentest");
-})
-app.get('/gentest',(req,res)=>{res.render("gentest.ejs");})
-
-app.post('/misc',(req,res)=>{
-    var t = req.body;
-    // console.log(t.x101);
-    if(t.x95=='')temp=95;
-    else if(t.x98=='98')temp=98;
-    else if(t.x101=='101')temp=101;
-    else if(t.x103=='103')temp=103;
-    else if(t.x105=='105')temp=105;
-    res.redirect('/misc');
-})
-app.get('/misc',(req,res)=>{
-    res.render('misc.ejs');
-})
-
-app.get('/gender',(req,res)=>{res.render("gender.ejs");})
 
 app.post('/for',(req,res)=>{
     name = req.body.fname+" "+req.body.sname;
     email = req.body.email;
     dob = req.body.age;
     age = getAge(dob);
-    console.log(age);
-    console.log(name);
-    console.log(email);
-    res.redirect('/for');
+    var newUser = new User({name:name,email:email,dob:dob,age:age,book1:'0',book2:'0',bookphy:'0',booktrain:'0'});
+    newUser.save((err,usr)=>{
+        if(err){
+            // console.log(err)
+            res.send("Error Page");
+        }
+        else{
+            id = usr._id;
+            next = '/for/'+id;
+            res.redirect(next);
+        }
+    })
 })
-app.get('/for',(req,res)=>{res.render("for.ejs");})
 
-app.post('/hw',(req,res)=>{
+app.get('/for/:id',(req,res)=>{
+    res.render("for.ejs",{id:req.params.id});
+})
+
+app.get('/gender/:id',(req,res)=>{
+    res.render("gender.ejs",{id:req.params.id});
+})
+
+app.post('/gentest/:id',(req,res)=>{
+    var gender = req.body;
+    if(gender.male=='male')sex='male';
+    else sex='female';
+    
+    User.findByIdAndUpdate(req.params.id,{sex:sex},function (err,update){
+        // console.log(update);
+        res.redirect("/gentest/"+req.params.id);
+    })
+})
+app.get('/gentest/:id',(req,res)=>{
+    console.log(req.params.id);
+    res.render("gentest.ejs",{id:req.params.id});
+})
+
+app.post('/hw/:id',(req,res)=>{
     ubp = req.body.ubp;
     lbp = req.body.lbp;
     gul = req.body.Glucose;
     chol = req.body.Cholesterol;
 
-    console.log(gul,chol);
+    // console.log(gul,chol);
 
     if(ubp<10||ubp>200)ubp=120;
     if(lbp<10||lbp>200)lbp=80;
-    res.redirect('/hw');
-})
-app.get('/hw',(req,res)=>{res.render("hw.ejs");})
 
-app.post('/temp',(req,res)=>{
+    User.findByIdAndUpdate(req.params.id,{ubp:ubp,lbp:lbp,gulc:gul,chol:chol},function (err,update){
+        // console.log(update);
+        res.redirect('/hw/'+req.params.id);
+    })
+})
+app.get('/hw/:id',(req,res)=>{res.render("hw.ejs",{id:req.params.id});})
+
+app.post('/temp/:id',(req,res)=>{
     hgt = req.body.hgt;
     wgt = req.body.wgt;
     h = hgt/100;
     bmi = (wgt/(h*h)).toFixed(2);
-    console.log(hgt,wgt);
-    res.redirect('/temp');
+    
+    User.findByIdAndUpdate(req.params.id,{height:hgt,weight:wgt,bmi:bmi},function(err,update){
+        // console.log(update);
+        res.redirect('/temp/'+req.params.id);
+    })
 })
-app.get('/temp',(req,res)=>{res.render("temp.ejs");})
+app.get('/temp/:id',(req,res)=>{res.render("temp.ejs",{id:req.params.id});})
 
-app.post('/symptoms',(req,res)=>{
+app.post('/misc/:id',(req,res)=>{
+    var t = req.body;
+    if(t.x95=='')temp=95;
+    else if(t.x98=='98')temp=98;
+    else if(t.x101=='101')temp=101;
+    else if(t.x103=='103')temp=103;
+    else if(t.x105=='105')temp=105;
+
+    User.findByIdAndUpdate(req.params.id,{temp:temp},function(err,update){
+        // console.log(update);
+        res.redirect('/misc/'+req.params.id);
+    })
+})
+app.get('/misc/:id',(req,res)=>{
+    res.render('misc.ejs',{id:req.params.id});
+})
+
+app.post('/symptoms/:id',(req,res)=>{
     smoke = req.body.smoke;
     alcohol = req.body.alcohol;
     physc = req.body.physc;
-    console.log(smoke,alcohol,physc);
+    // console.log(smoke,alcohol,physc);
 
     ag = age*365;
     gd = sex === 'male'? 2 : 1;
@@ -117,14 +253,42 @@ app.post('/symptoms',(req,res)=>{
     sm = smoke==='yes'?1:0;
     al = alcohol==='yes'?1:0;
     ph = physc==='yes'?1:0;
-    res.redirect('/symptoms');
+
+    User.findById(req.params.id,function(err,found){
+        ag = found.age * 365;
+        gd = found.sex === 'male'? 2 : 1;
+        if(found.chol==='norm')ch=1;
+        else if(found.chol==='abvnor')ch=2;
+        else ch=3;
+        if(found.gulc==='norm')gl=1;
+        else if(found.gulc==='abvnor')gl=2;
+        else gl=3;
+    })
+
+    User.findByIdAndUpdate(req.params.id,{ag:ag,gd:gd,ch:ch,gul:gl,sm:sm,al:al,ph:ph},function(err,update){
+        res.redirect('/symptoms/'+req.params.id);
+    })
 })
-app.get('/symptoms',(req,res)=>{
-    console.log(ag,hg,wi,gd,sb,db,ch,gl,sm,al,ph,"11 done");
-    res.render("symptoms.ejs",{ag:ag,hg:hg,wi:wi,gd:gd,sb:sb,db:db,ch:ch,gl:gl,sm:sm,al:al,ph:ph});
+app.get('/symptoms/:id',(req,res)=>{
+    User.findById(req.params.id,function(err,found){
+        if(found){
+            res.render("symptoms.ejs",{ag:found.ag,hg:found.height,wi:found.weight,gd:found.gd,sb:found.ubp,db:found.lbp,ch:found.ch,gl:found.gul,sm:found.sm,al:found.al,ph:found.ph,id:req.params.id});
+        }
+        else{
+            res.send("Error Page");
+        }
+    })
 })
 
-app.post("/results",(req,res)=>{
+app.post('/addsymp/:id',(req,res)=>{
+    Symp.create({symptom:req.body.symptom},function(){});
+    res.redirect('/addsymp/'+req.params.id);
+})
+app.get('/addsymp/:id',(req,res)=>{
+    res.render('addsymp.ejs',{id:req.params.id});
+})
+
+app.post("/results/:id",(req,res)=>{
     data=[];perc=[];ab=[];
     data.push(req.body.a);perc.push(req.body.a1);
     data.push(req.body.b);perc.push(req.body.b1);
@@ -134,19 +298,116 @@ app.post("/results",(req,res)=>{
     ab.push(req.body.f1);ab.push(req.body.f2);
 
     data2 = req.body.new;
-    console.log(data2);
+    // console.log(data2);
 
     // console.log(req.body);
 
-    console.log(name,age,email,sex,hgt,wgt,temp,dob,smoke,alcohol,physc,bmi);
-    console.log(ubp,lbp,gul,chol);
+    // console.log(name,age,email,sex,hgt,wgt,temp,dob,smoke,alcohol,physc,bmi);
+    // console.log(ubp,lbp,gul,chol);
 
-    res.redirect('/results');
+    res.redirect('/results/'+req.params.id);
 })
-app.get('/results',(req,res)=>{
-    res.render("result",{name:name,age:age,dob:dob,email:email,sex:sex,bmi:bmi,result:data,perc:perc,ab:ab,data2:data2});
+
+
+
+app.get('/results/:id',(req,res)=>{
+    User.findById(req.params.id,function(err,found){
+        if(found){
+            res.render("result",{name:found.name,age:found.age,dob:found.dob,email:found.email,sex:found.sex,bmi:found.bmi,
+                result:data,perc:perc,ab:ab,data2:data2,book1:found.book1,book2:found.book2,bookphy:found.bookphy,booktrain:found.booktrain,
+                id:req.params.id    
+            });
+        }
+        else{res.send("Error Page");}
+    })
+})
+
+app.post('/slot/:id/:deprt',(req,res)=>{
+    // console.log(req.body);
+    Appoint.findOne({Deprt:req.params.deprt},function(err,found){
+        found.slot.push(parseInt(req.body.name));
+        found.save(()=>{
+            User.findById(req.params.id,function(err,found){
+                tm = CTime(parseInt(req.body.name));
+                if(req.params.deprt==='General Physician'){
+                    found.bookphy = tm;
+                    found.save(()=>{});
+                }
+                else if(req.params.deprt==='Physical Trainer'){
+                    found.booktrain = tm;
+                    found.save(()=>{});
+                }
+                else if(found.book1.length<3){
+                    found.book1 = tm;
+                    found.save(()=>{});
+                }
+                else{
+                    found.book2 = tm;
+                    found.save(()=>{});
+                }
+            })
+        });
+    });
+    res.redirect('/results/'+req.params.id);
+})
+
+app.get('/slot/:id/:deprt',(req,res)=>{
+    // Appoint.find({},(e,f)=>{console.log(f);})
+    console.log(req.params.deprt);
+    Appoint.findOne({Deprt:req.params.deprt},function(err,f){
+        console.log(f);
+        if(f){
+            res.render('slot.ejs',{id:req.params.id,deprt:req.params.deprt,slot:f.slot});
+        }
+        else{res.send('Error Page');}
+    })
 })
 
 app.listen(process.env.PORT||3000,process.env.IP,()=>{
     console.log("Server");
 })
+
+
+
+
+// var department = ['Otolaryngologist',
+// 'Immunologists',
+// 'Dermatologist',
+// 'Hepatologist',
+// 'Allergist',
+// 'Rheumatologist',
+// 'Pulmonologist',
+// 'Orthopaedists',
+// 'Infectious disease specialist',
+// 'Hepatologist',
+// 'General physician',
+// 'General physician',
+// 'Endocrinologist',
+// 'Gastroenterologist',
+// 'Allergist',
+// 'Infectious disease specialist',
+// 'Otolaryngologist',
+// 'Gastroenterologist',
+// 'Cardiologist',
+// 'Hepatologist',
+// 'Hepatologist',
+// 'Hepatologist',
+// 'Hepatologist',
+// 'General practitioner',
+// 'Endocrinologist',
+// 'Endocrinologist',
+// 'Endocrinologist',
+// 'General practitioner',
+// 'Gastroenterologist',
+// 'Infectious disease specialist',
+// 'General practitioner',
+// 'Orthopaedician',
+// 'Neurologist',
+// 'Gastroenterologist',
+// 'Pulmonologist',
+// 'Dermatologist',
+// 'Infectious disease specialist',
+// 'General practitioner',
+// 'Urologist',
+// 'Cardiologist',
+// 'Hepatologist'];
